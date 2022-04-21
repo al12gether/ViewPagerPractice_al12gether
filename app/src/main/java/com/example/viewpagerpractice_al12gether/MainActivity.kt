9c12ca49e1492e9a65c72637f1fddb14b83bbfd8
al12gether <alonetogetherpd@gmail.com>
2022-04-21 15:37:02 +0900

    추가적 페이지 생성시 대응 코드 추가

--- app/src/main/java/com/example/viewpagerpractice_al12gether/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerpractice_al12gether/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.viewpagerpractice_al12gether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mViewPagerAdapter
+
+//        페이지가 많아지면 간단하게 부드럽도록 지원하는 기능.
+        mainViewPager.offscreenPageLimit = 5
+
     }
 }
--- app/src/main/java/com/example/viewpagerpractice_al12gether/fagments/NameFragments.kt
+++ app/src/main/java/com/example/viewpagerpractice_al12gether/fagments/NameFragments.kt
@@ -14,7 +14,7 @@ class NameFragments : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.f, container, false)
+        return inflater.inflate(R.layout. fragment_name, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
