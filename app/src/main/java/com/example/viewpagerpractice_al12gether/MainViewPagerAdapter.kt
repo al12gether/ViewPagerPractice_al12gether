674496a6082ddc2cace44b2ec03e11e11600140a
al12gether <alonetogetherpd@gmail.com>
2022-04-21 17:07:32 +0900

    코틀린 조건문인 when 활용 예시

--- app/src/main/java/com/example/viewpagerpractice_al12gether/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice_al12gether/MainViewPagerAdapter.kt
@@ -10,30 +10,26 @@ import com.example.viewpagerpractice_al12gether.fagments.NameFragments
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
-        if (position == 0) {
-            return "이름"
+
+        return when (position) {
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
         }
-        else if (position == 1) {
-            return "내 정보"
-        }
-        else {
-            return  "인사말"
-        }}
+
+    }
 
     override fun getCount(): Int {
         return   3
     }
 
     override fun getItem(position: Int): Fragment {
-      if (position ==0) {
-          return NameFragments()
-    }
-      else if (position ==1)  {
-          return MyinfoFragments()
-      }
-      else {
-          return GreetingFragments()
-      }
+
+        return when(position) {
+            0 -> NameFragments()
+            1 -> MyinfoFragments()
+            else -> GreetingFragments()
+        }
 
 }
 
